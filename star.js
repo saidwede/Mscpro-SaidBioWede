e29e1c82971e76fc5a94106ac95b063032281c66
SAID BIO WEDE <saidbiowede@gmail.com>
2023-07-26 13:16:50 +0100

    Update star.js

--- star.js
+++ star.js
@@ -1,3 +1,4 @@
+//console.log("***********");
 process.argv.forEach(function (val, index, array) {
     //console.log(index + ': ' + val);
 });
@@ -7,28 +8,28 @@ let totalLine = 0;
 let starMap = [];
 
 size = parseInt(process.argv[2]);
-let firstStartDec = 3*size;
+let firstStartDec = (6*size)/2;
 totalLine = 4*size + 1;
 
 for (let index = 0; index < totalLine; index++) {
-    let emptyLine = [size*5 + 1];
-    for (let index = 0; index < size*5 + 1; index++) {
-        emptyLine [index] =  " ";
+    let emptyLine = [];
+    for (let index0 = 0; index0 < size*6+1 ; index0++) {
+        emptyLine[index0] = " ";
     }
-    starMap.push(emptyLine);    
+    starMap.push(emptyLine);
 }
-firstStartDec = starMap.length - 1 
+//firstStartDec = starMap.length - 1 
 for (let index = 0; index < starMap.length; index++) {
     if(index === 0){
-        starMap[0][firstStartDec-size] = "*";
+        starMap[0][firstStartDec] = "*";
     }else{
         if(index < size){
-            starMap[index][firstStartDec-index-size] = "*";
-            starMap[index][firstStartDec+index-size] = "*";
+            starMap[index][firstStartDec-index] = "*";
+            starMap[index][firstStartDec+index] = "*";
         }
         if(index == size){
             for (let index2 = 0; index2 < starMap[index].length; index2++) {
-                if(index2 <= (firstStartDec-size-size) || index2 >= (firstStartDec+size-size)){
+                if(index2 <= (firstStartDec-index) || index2 >= (firstStartDec+index)){
                     starMap[index][index2] = "*";
                 }
                 
